33fd2b1881aa260287234daac75e8392bb08180d
Changeryto <changeryto@hotmail.com>
2024-08-11 06:47:37 -0600

    Se aniaden comentarios a 10-punteros

--- 10-punteros/main.cpp
+++ 10-punteros/main.cpp
@@ -6,6 +6,11 @@ int main() {
     char letra = 'A';
     char *puntero = &letra;
 
+    // & solicita la direccion de memoria
+    // * indica que es un puntero
+    // (int *) indica que una direccion de memoria debe formatearse como tal.
+    // *puntero solicita el valor al que se apunta
+
     cout << "Letra: " << letra << endl;
     cout << "Direccion en memoria de letra: " << &letra << endl;
     cout << "Direccion de memoria convertido: " << (int *) &letra << endl;
